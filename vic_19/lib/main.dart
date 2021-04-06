b3a513422c37da376162253c1a9e6982535e5109
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-04-06 13:49:49 -0400

    probando la subida de cambios al github

--- vic_19/lib/main.dart
+++ vic_19/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vic-19',
+      title: 'Vic-19  prueba',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
