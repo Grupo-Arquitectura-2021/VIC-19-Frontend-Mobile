b5a0ca41c3fed4cde7a344da5991ec8a9ebf4ee6
KevinEs14 <escobarkev@gmail.com>
2021-04-10 21:54:35 -0400

    creation of the menu with a page for the colors that will be use in all the app

---
+++ vic_19/lib/Colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+var color1 = Color(0xFF2E363E);
+// var color1=Colors.white;
+var color2 = Color(0xFF7BC950);
+var color3 = Color(0xFFF09D51);
+var color4 = Color(0xFFF06543);
+var color5 = Color(0xFFB1B8BD);
+var color6 = Color(0xFF247BA0);
---
+++ vic_19/lib/Pages/BottomMenu.dart
@@ -0,0 +1,59 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../Colors.dart';
+
+class BottomMenu extends StatefulWidget {
+  @override
+  _BottomMenuState createState() => _BottomMenuState();
+}
+
+class _BottomMenuState extends State<BottomMenu> {
+  int _selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _selectedIndex,
+        items: [
+          Icon(
+            Icons.analytics_outlined,
+            // Icons.assessment_outlined,
+            size: 40,
+            color: Colors.white,
+          ),
+          Icon(
+            // Icons.menu,
+            // Icons.home_rounded,
+            Icons.public_rounded,
+            size: 40,
+            color: Colors.white,
+          ),
+          Icon(
+            // Icons.add_chart,
+            // Icons.article_outlined,
+            Icons.list_alt_rounded,
+            size: 40,
+            color: Colors.white,
+          ),
+          // Icon(Icons.add_shopping_cart,size: 40,),
+        ],
+        onTap: (index){
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        buttonBackgroundColor: color2,
+        backgroundColor: color1,
+        color: color5,
+        animationCurve: Curves.easeInBack,
+        animationDuration: const Duration(milliseconds: 300),
+      ),
+      body: Container(
+        color: color1,
+      ),
+    );
+  }
+}
