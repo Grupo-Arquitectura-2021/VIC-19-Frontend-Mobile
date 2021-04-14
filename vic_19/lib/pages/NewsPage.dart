c5e9261ff17bb3be0906e4a0559fcb70d1a5c510
KevinEs14 <escobarkev@gmail.com>
2021-04-14 03:13:07 -0400

    implementation of cards in news page and creation of alertdialogs on the onTap

--- vic_19/lib/pages/NewsPage.dart
+++ vic_19/lib/pages/NewsPage.dart
@@ -7,8 +7,36 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
+  int idNews;
+  Future<void> _showMyDialog(int idNews) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('News',style: TextStyle(color: color3),),
+          backgroundColor: color1,
+          content: SingleChildScrollView(
+            child: Text("loremsafjlafjlasajfldsjalfjlsajfljakfl;jasl;fjklasjfl;asjkfkshafklhajsfkljhaskfhklshfksahfklhjaskd"
+                "fhaksfhkasfksahfklhsajfhkashfkhsakfhaskjfhkashdfkahsdkfhkaslhdfkashfklhaskdfjhksadjfhkasjhdfjklahfdklsahfkljhsk"
+                "lfjhkalshdfkashdfklsahfklsahdfklhsdfklhsdjkhfksadhfjkjsahdfjkashfkahskljfhasklfhklsahfklashfjkdlshaklfhjjkslajhfjklsahdf"
+                "fhaksfhkasfksahfklhsajfhkashfkhsakfhaskjfhkashdfkahsdkfhkaslhdfkashfklhaskdfjhksadjfhkasjhdfjklahfdklsahfkljhsk"
+                "fhaksfhkasfksahfklhsajfhkashfkhsakfhaskjfhkashdfkahsdkfhkaslhdfkashfklhaskdfjhksadjfhkasjhdfjklahfdklsahfkljhsk",style: TextStyle(color: color5),),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Aceptar',style: TextStyle(color: color4),),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
   @override
-  List<String> _news=['prueba','prueba2','prueba3','prueba4','prueba5','prueba6'];
+  List<String> _news=['breve descripcion','breve descripcion 2','breve descripcion 3','breve descripcion 4','breve descripcion 5','breve descripcion 6'];
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Container(
@@ -17,19 +45,36 @@ class _NewsPageState extends State<NewsPage> {
           itemCount: _news.length,
             scrollDirection: Axis.vertical,
             itemBuilder: (context,index){
-              return Card(
-                color: color3,
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Container(
-                  height: size.height*0.1,
-                  child: Column(
-                    children: [
-                      Text("Title"),
-                      Text("${_news[index]}"),
-                    ],
+              return GestureDetector(
+                onTap: (){
+                  _showMyDialog(idNews);
+                  // print("touch");
+                  // setState(() {
+                  //   SpecificNews(idNews);
+                  // });
+                  // SpecificNews(idNews);
+                },
+                child: Card(
+                  color: color1,
+                  elevation: 5,
+                  borderOnForeground: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Container(
+                    height: size.height*0.12,
+                    // decoration: BoxDecoration(
+                    //   borderRadius: BorderRadius.circular(10.0),
+                    //   border: Border.all(color: color2)
+                    // ),
+                    // child: Center(child: Text("Title",style: TextStyle(color: color3,fontSize: size.height*0.035),)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Title",style: TextStyle(color: color3,fontSize: size.height*0.035),),
+                        Text("${_news[index]}",style: TextStyle(color: color5,fontSize: size.height*0.02),),
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -37,4 +82,20 @@ class _NewsPageState extends State<NewsPage> {
         )
       );
   }
+
+  Widget SpecificNews(int idNews){
+    return AlertDialog(
+      title: Text("News",style: TextStyle(color: color5,fontSize: 20),),
+      content: SingleChildScrollView(
+        child: Text("loremsafjlafjlasajfldsjalfjlsajfljakfl;jasl;fjklasjfl;asjkfkshafklhajsfkljhaskfhklshfksahfklhjaskd"
+            "fhaksfhkasfksahfklhsajfhkashfkhsakfhaskjfhkashdfkahsdkfhkaslhdfkashfklhaskdfjhksadjfhkasjhdfjklahfdklsahfkljhsk"
+            "lfjhkalshdfkashdfklsahfklsahdfklhsdfklhsdjkhfksadhfjkjsahdfjkashfkahskljfhasklfhklsahfklashfjkdlshaklfhjjkslajhfjklsahdf"),
+      ),
+      actions: <Widget>[
+        TextButton(onPressed:(){Navigator.of(context).pop();} , child: Text("Aceptar")),
+      ],
+    );
+  }
 }
+
+
